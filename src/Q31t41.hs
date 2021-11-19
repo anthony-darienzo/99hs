af17ca8fbd8b6c126778e5935e875870cb113480
Anthony D'Arienzo <anthony384@outlook.com>
2021-11-19 16:39:52 -0600

    Initial goldbach commit

--- src/Q31t41.hs
+++ src/Q31t41.hs
@@ -13,7 +13,7 @@ tests 35 = primeFactors 315 == [3,3,5,7]
 tests 36 = primeFactorsMult 315 == [(3,2),(5,1),(7,1)]
 tests 37 = totient' 10 == 4
 tests 39 = primesR 10 20 == [11,13,17,19]
-tests 40 = goldbach 28 == (5,23) 
+tests 40 = goldbach 28 == (5,23)
 tests n
     | n <= 41 = True
     | otherwise = False
@@ -21,14 +21,23 @@ tests n
 -- Problem 31
 -- Determine whether a given integer number is prime
 isPrime :: Int -> Bool
-isPrime n 
+isPrime n
     | n == 2 = True
     | n > 2 = null [d | d <- [2..m], n `mod` d == 0]
     | otherwise = False
     where m = ceiling . sqrt $ fromIntegral n
     -- We need to separate the n=2 case because ceiling . sqrt $ fromIntegral 2
     -- is 2, so [d | d <- [2..m] ...] is nonempty.
+
 -- The Sieve of Eratosthenes would speed a lot of these algorithms up.
+-- We will create the list of primes naively, though.
+primesR :: Int -> Int -> [Int]
+primesR m n
+    | m <= n = thePrimes
+    | otherwise = reverse $ primesR n m
+    where thePrimes = [p | p <- [m..n] , isPrime p]
+primes :: [Int]
+primes = [p | p <- [2..], isPrime p]
 
 -- Problem 32
 -- Find the gcd of two positive integers via Euclid's algorithm.
@@ -60,9 +69,9 @@ primeFactors n
     | n <= 1 = []
     | isPrime n = [n]
     | otherwise = p0 : primeFactors ( n `div` p0)
-    where 
+    where
         m = ceiling . sqrt $ fromIntegral n
-        p0 = head [p | p <- [2..m], isPrime p && n `mod` p == 0]
+        p0 = head [p | p <- primes, n `mod` p == 0]
 
 -- Problem 36
 -- Determine prime factors and give their multiplicity
@@ -78,7 +87,7 @@ primeFactorsMult = map swap . concatMap clean . encode . primeFactors
 -- Problem 37
 -- Use Problem 36 to calculate the totient
 totientFormula :: [(Int,Int)] -> Int
-totientFormula = 
+totientFormula =
     foldr (\ (p,m) acc -> acc * ( (p - 1) * p ^ (m - 1) ) ) 1
 
 totient' :: Int -> Int
@@ -92,19 +101,16 @@ totient' = totientFormula . primeFactorsMult
 
 -- Problem 39
 -- List the prime numbers from m to n
-primesR :: Int -> Int -> [Int]
-primesR m n
-    | m <= n = thePrimes
-    | otherwise = reverse $ primesR n m
-    where thePrimes = [p | p <- [m..n] , isPrime p]
+-- See primesR above
 
 -- Problem 40
 -- Given an even integer, find two primes which sum to it
-
--- The sieve of Eratosthenes sounds nice now.
-
 goldbach :: Int -> (Int,Int)
-goldbach = error "Not Implemented"
+goldbach n
+    | even n && n > 2 = head [s | s <- filteredPrimes, uncurry (+) s == n]
+    | otherwise = (0,0) -- For odd numbers we just return something
+    where
+        filteredPrimes = [(p,n - p) | p <- primes, isPrime (n-p)]
 
 -- Problem 41
 -- Given a range of integers m and n, print the goldbach decompositions
